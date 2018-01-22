8fbf8e2e168d476d7dc6298635dfcabcd823a313
CM Student <cmstudent@UHWO-ACM-18.local>
2018-01-22 10:53:16 -1000

    Completed Hackwich 2 Added two IBactions

--- Hackwich2/ViewController.swift
+++ Hackwich2/ViewController.swift
@@ -20,6 +20,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func PressMeButtonPressed(_ sender: Any) {
+//codeblock
+        print("Hello World")
+    }
 
+    @IBAction func PressMeNowPressed(_ sender: Any) {
+        print("Hello everyone")
+    }
 }
 
